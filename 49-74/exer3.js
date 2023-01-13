e412c4bd0b5c403af00daac7ecf08a525fc22edd
some user <user.19933@bk.ru>
2023-01-13 17:25:57 +0300

    add closure

---
+++ 49-74/exer3.js
@@ -0,0 +1,47 @@
+
+{
+    console.log("Closure sum")
+    // Сумма с помощью замыканий
+    function sum(a) {
+
+        return function(b) {
+            return a + b; // берёт "a" из внешнего лексического окружения
+        };
+
+    }
+
+    console.log( sum(1)(2) ); // 3
+    console.log( sum(5)(-1) ); // 4
+
+}
+
+{
+    console.log("by field")
+    //сортировка по полю
+    let users = [
+        { name: "John", age: 20, surname: "Johnson" },
+        { name: "Pete", age: 18, surname: "Peterson" },
+        { name: "Ann", age: 19, surname: "Hathaway" }
+    ];
+
+    /*
+    usual
+    // по имени (Ann, John, Pete)
+users.sort((a, b) => a.name > b.name ? 1 : -1);
+
+// по возрасту (Pete, Ann, John)
+users.sort((a, b) => a.age > b.age ? 1 : -1);
+     */
+
+
+    function byField(field) {
+        return (a, b) => a[field] > b[field] ? 1 : -1;
+    }
+
+    users.sort(byField('name'));
+    users.forEach(user => console.log(user.name)); // Ann, John, Pete
+
+    users.sort(byField('age'));
+    users.forEach(user => console.log(user.name)); // Pete, Ann, John
+
+}
